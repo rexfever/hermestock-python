35dec0be8f7d95b4eb02251f0085835495321afa
rexfever <rex.fever.sohn@gmail.com>
2020-12-06 20:23:32 +0900

    update downloader, update calc_rank

--- downloader.py
+++ downloader.py
@@ -5,28 +5,29 @@ from datetime import datetime
 
 '''
 1. 목적페이지 이동 (function화)
-2. 날짜 세팅 (function화)
+2. 날짜 세팅 
 3. 시장 선택 (function화)
 4. 투자자 선택 후 다운로드(function화)
 4. 시장 수만큼 3번째 항 반복
 '''
 DRIVER = webdriver.Chrome('./chromedriver')
 
-# 시장, 매수주 선언
+# 시장, 매수주체 선언
 MARKETS = [['3', 'kospi'], ['5', 'kosdaq']]
 BUYERS = [['7050', 'KIK'], ['6000', 'YEON'], ['9000', 'FO']]
 
 
-# 날짜 세팅
+# 날짜 및 검색 항목 설정
 def _set_date(driver):
     driver.get('http://marketdata.krx.co.kr/mdi#document=040404')
     sleep(5)
+    driver.find_element_by_css_selector(
+        f'.design-fieldset > form > dl:nth-of-type(4) > dd > input:nth-child(3)').click()
     target_date = driver.find_element_by_name('schdate')
     target_date.clear()
     target_date.send_keys(datetime.now().strftime('%Y%m%d'))
 
-
-# 매수 주체 선택 후 다운로
+# 매수 주체 선택 후 다운로드
 def _select_buyer(element, buyers, driver):
     for buyer in buyers:
         element.select_by_value(buyer[0])
@@ -42,6 +43,8 @@ def _select_market(markets, driver):
         driver.find_element_by_css_selector(f'.design-fieldset > form > dl > dd > input:nth-child({market[0]})').click()
         select_element_id = driver.find_element_by_name('var_invr_cd').get_attribute("id")
         selected_element = Select(driver.find_element_by_id(select_element_id))
-        _select_buyer(selected_element, BUYERS, DRIVER)
+        _select_buyer(selected_element, BUYERS, driver)
 
 
+_set_date(DRIVER)
+_select_market(MARKETS, DRIVER)
