dfa152cae6844b72d83b0770b978c0c6145c8849
Jinhyoung Sohn <jinhyoung.sohn@deliveryhero.co.kr>
2020-12-10 15:53:03 +0900

    add main.py and update ithers

--- downloader.py
+++ downloader.py
@@ -18,34 +18,31 @@ BUYERS = [['7050', 'KIK'], ['9000', 'FO']]
 
 
 # 날짜 및 검색 항목 설정
-def _set_date(driver):
-    driver.get('http://marketdata.krx.co.kr/mdi#document=040404')
+def _set_date():
+    DRIVER.get('http://marketdata.krx.co.kr/mdi#document=040404')
     sleep(5)
-    driver.find_element_by_css_selector(
+    DRIVER.find_element_by_css_selector(
         f'.design-fieldset > form > dl:nth-of-type(4) > dd > input:nth-child(3)').click()
-    target_date = driver.find_element_by_name('schdate')
+    target_date = DRIVER.find_element_by_name('schdate')
     target_date.clear()
     target_date.send_keys(datetime.now().strftime('%Y%m%d'))
 
 
 # 매수 주체 선택 후 다운로드
-def _select_buyer(element, buyers, driver):
-    for buyer in buyers:
+def _select_buyer(element):
+    for buyer in BUYERS:
         element.select_by_value(buyer[0])
-        driver.find_element_by_class_name('btn-board.btn-board-search').click()
-        sleep(2)
-        excel_button = driver.find_element_by_xpath("//*[contains(text(), 'CSV')]")
+        DRIVER.find_element_by_class_name('btn-board.btn-board-search').click()
+        sleep(3)
+        excel_button = DRIVER.find_element_by_xpath("//*[contains(text(), 'CSV')]")
         excel_button.click()
 
 
 # 시장 선택
-def _select_market(markets, buyers, driver):
-    for market in markets:
-        driver.find_element_by_css_selector(f'.design-fieldset > form > dl > dd > input:nth-child({market[0]})').click()
-        select_element_id = driver.find_element_by_name('var_invr_cd').get_attribute("id")
-        selected_element = Select(driver.find_element_by_id(select_element_id))
-        _select_buyer(selected_element, buyers, driver)
+def _select_market():
+    for market in MARKETS:
+        DRIVER.find_element_by_css_selector(f'.design-fieldset > form > dl > dd > input:nth-child({market[0]})').click()
+        select_element_id = DRIVER.find_element_by_name('var_invr_cd').get_attribute("id")
+        selected_element = Select(DRIVER.find_element_by_id(select_element_id))
+        _select_buyer(selected_element)
 
-
-_set_date(DRIVER)
-_select_market(MARKETS, BUYERS, DRIVER)
---
+++ main.py
@@ -0,0 +1,22 @@
+import downloader as dl
+from time import sleep
+import calc_rank as cr
+import os
+import glob
+import getpass
+
+
+
+KOSPI_fileList = ['data.csv', 'data (1).csv']
+KOSDAQ_fileList = ['data (2).csv', 'data (3).csv']
+
+
+dl._set_date()
+dl._select_market()
+sleep(7)
+
+print(cr._merge_data_set(cr._extract_data_set(KOSPI_fileList[0]), cr._extract_data_set(KOSPI_fileList[1])))
+print(cr._merge_data_set(cr._extract_data_set(KOSDAQ_fileList[0]), cr._extract_data_set(KOSDAQ_fileList[1])))
+
+
+[os.remove(f) for f in glob.glob("/Users/"+getpass.getuser()+"/Downloads/*.csv")]
