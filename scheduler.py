d405f1516a7d917323cec58a4ca559fe6c2649a7
rexfever <rex.fever.sohn@gmail.com>
2021-03-01 00:13:47 +0900

    use headless fix4

--- scheduler.py
+++ scheduler.py
@@ -7,10 +7,11 @@ def job():
     hermes.job()
 
 
-#schedule.every().minute.do(job)
+# schedule.every().minute.do(job)
 schedule.every().days.at("15:45").do(job)
 
 while True:
+    print(time.localtime().tm_hour + ":" + time.localtime().tm_min + ":" + time.localtime().tm_sec)
     now = time.localtime().tm_wday
     if now != 5 and now != 6:
         schedule.run_pending()
