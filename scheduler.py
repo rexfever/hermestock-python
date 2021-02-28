a7de611aaef1094bedc4b06643000fa04ce0ef98
rexfever <rex.fever.sohn@gmail.com>
2021-03-01 00:23:41 +0900

    use headless fix5

--- scheduler.py
+++ scheduler.py
@@ -11,7 +11,7 @@ def job():
 schedule.every().days.at("15:45").do(job)
 
 while True:
-    print(time.localtime().tm_hour + ":" + time.localtime().tm_min + ":" + time.localtime().tm_sec)
+    print(f'{time.localtime().tm_year}.{time.localtime().tm_mon}.{time.localtime().tm_mday} {time.localtime().tm_hour}:{time.localtime().tm_min}:{time.localtime().tm_sec}')
     now = time.localtime().tm_wday
     if now != 5 and now != 6:
         schedule.run_pending()
